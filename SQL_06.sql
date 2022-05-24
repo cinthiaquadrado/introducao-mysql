f304896a4cde8723ff7339b24793ab5a25eb3d5e
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-23 23:22:34 -0300

    Arquivos de exercícios da aula

---
+++ SQL_06.sql
@@ -0,0 +1,7 @@
+UPDATE tabela_de_vendedores SET PERCENTUAL_COMISSAO = '0.11'
+WHERE MATRICULA = '00236';
+
+UPDATE tabela_de_vendedores SET NOME = 'José Geraldo da Fonseca Junior'
+WHERE MATRICULA = '00233';
+
+select * from tabela_de_vendedores;
---
+++ SQL_16.sql
@@ -0,0 +1,11 @@
+SELECT * FROM tbproduto;
+
+SELECT * FROM tbproduto WHERE PRECO_LISTA >= 16.007 AND PRECO_LISTA <= 16.009;
+
+SELECT * FROM tbcliente WHERE IDADE >= 18 AND IDADE <= 22 AND SEXO = 'M';
+
+SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro' OR BAIRRO = 'Jardins';
+
+SELECT * FROM tbcliente WHERE (IDADE >= 18 AND IDADE <= 22) OR (CIDADE = 'Rio de Janeiro' OR BAIRRO = 'Jardins');
+
+SELECT * FROM tabela_de_vendedores WHERE (DE_FERIAS = 1) AND (YEAR(DATA_ADMISSAO) < 2016);
